6c3400f2afda21d771ae9fcc2abe12b0064f0dcc
kwankurn <90328302+heryyy@users.noreply.github.com>
2024-06-03 17:17:35 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,22 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
- 
+const { connectDB, sequelize } = require('./db');
+
 const init = async () => {
+    await connectDB(); // Connect to MySQL
+
+    // Sync all models that aren't already in the database
+    await sequelize.sync({ alter: true });
+
     const server = Hapi.server({
         port: 8000,
         host: 'localhost',
     });
- 
+
     server.route(routes);
- 
+
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
+
 init();
