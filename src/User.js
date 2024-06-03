1fac49616a572d1a72b47d9f5e6e2d4055e242f2
kwankurn <90328302+heryyy@users.noreply.github.com>
2024-06-03 17:33:13 +0700

    Create User.js

---
+++ src/User.js
@@ -0,0 +1,28 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const { sequelize } = require('../db');
+
+const User = sequelize.define('User', {
+    fullName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+    },
+});
+
+module.exports = User;
